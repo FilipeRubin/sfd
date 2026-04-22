52a1f8fcf19dc648a76d68c13059d4de8a8cb427
Rubin <filiperubin@gmail.com>
2026-04-22 05:08:55 -0300

    Create FixedArray templated container type

---
+++ core-systems/include/containers/fixed-array.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <cstring>
+
+template<typename T>
+class FixedArray
+{
+public:
+	FixedArray(size_t elementCount) :
+		m_data(new T[elementCount]),
+		m_elementCount(elementCount)
+	{
+	}
+
+	FixedArray(const FixedArray& other) :
+		m_data(new T[other.m_elementCount]),
+		m_elementCount(other.m_elementCount)
+	{
+		std::memcpy(m_data, other.m_data, m_elementCount * sizeof(T));
+	}
+
+	FixedArray(FixedArray&& other) noexcept :
+		m_data(other.m_data),
+		m_elementCount(other.m_elementCount)
+	{
+		other.m_data = nullptr;
+		other.m_elementCount = 0ULL;
+	}
+
+	~FixedArray()
+	{
+		delete[] m_data;
+	}
+
+	FixedArray& operator=(const FixedArray& other)
+	{
+		if (this != &other)
+		{
+			delete[] m_data;
+			
+			m_data = new T[other.m_elementCount];
+			m_elementCount = other.m_elementCount;
+			std::memcpy(m_data, other.m_data, m_elementCount * sizeof(T));
+		}
+		return *this;
+	}
+
+	FixedArray& operator=(FixedArray&& other) noexcept
+	{
+		if (this != &other)
+		{
+			m_data = other.m_data;
+			m_elementCount = other.m_elementCount;
+			
+			other.m_data = nullptr;
+			other.m_elementCount = 0ULL;
+		}
+		return *this;
+	}
+
+	T& operator[](size_t index) noexcept
+	{
+		return m_data[index];
+	}
+
+	const T& operator[](size_t index) const noexcept
+	{
+		return m_data[index];
+	}
+
+	size_t GetElementCount() const
+	{
+		return m_elementCount;
+	}
+
+	T* GetData()
+	{
+		return m_data;
+	}
+
+	const T* GetData() const
+	{
+		return m_data;
+	}
+private:
+	T* m_data;
+	size_t m_elementCount;
+};
